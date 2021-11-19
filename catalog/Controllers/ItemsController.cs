f2a5a0c5efc2cc41c36c91f46db815d1a98f213c
Ezekiel <eabodesegun@appzonegroup.com>
2021-11-19 20:20:15 +0100

    add secrets, healthcare and docker file

--- catalog/Controllers/ItemsController.cs
+++ catalog/Controllers/ItemsController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Catalog.Dtos;
 using Catalog.Entities;
 using Catalog.Repositories;
@@ -21,15 +22,16 @@ namespace Catalog.Controllers
         }
 
          [HttpGet]
-        public IEnumerable<ItemDto> GetItems(){
-            var items = repository.GetItems().Select(item => item.AsDto());
+        public async Task<IEnumerable<ItemDto>> GetItemsAsync(){
+            var items = (await repository.GetItemsAsync())
+                        .Select(item => item.AsDto());
             return items;
         }
         
         //GET /items/{id}
         [HttpGet("{id}")]
-        public ActionResult<ItemDto> GetItem(Guid id){
-            var item = repository.GetItem(id);
+        public async Task<ActionResult<ItemDto>> GetItemAsync(Guid id){
+            var item = await repository.GetItemAsync(id);
             if(item is null){
                 return NotFound();
             }
@@ -37,21 +39,21 @@ namespace Catalog.Controllers
         }
         // POST /items
         [HttpPost]
-        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto){
+        public async Task<ActionResult<ItemDto>> CreateItemAsync(CreateItemDto itemDto){
             Item item = new(){
                 Id = Guid.NewGuid(),
                 Name = itemDto.Name,
                 Price = itemDto.Price,
                 CreatedDate = DateTimeOffset.UtcNow
             };
-            repository.CreateItem(item);
-            return CreatedAtAction(nameof(GetItem), new {id = item.Id}, item.AsDto());
+            await repository.CreateItemAsync(item);
+            return CreatedAtAction(nameof(GetItemAsync), new {id = item.Id}, item.AsDto());
         }
         // PUT /items/{id}
 
         [HttpPut("{id}")]
-        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto){
-            var existingItem = repository.GetItem(id);
+        public async Task<ActionResult> UpdateItemAsync(Guid id, UpdateItemDto itemDto){
+            var existingItem = await repository.GetItemAsync(id);
             if(existingItem is null){
                 return NotFound();
             }
@@ -59,18 +61,18 @@ namespace Catalog.Controllers
                 Name = itemDto.Name,
                 Price = itemDto.Price,
             };
-            repository.UpdateItem(updateItem);
+           await repository.UpdateItemAsync(updateItem);
             return NoContent();
         }
 
         // DELETE /items/{id}
         [HttpDelete("{id}")]
-        public ActionResult DeleteItem(Guid id){
-            var existingItem = repository.GetItem(id);
+        public async Task<ActionResult> DeleteItemAsync(Guid id){
+            var existingItem = await repository.GetItemAsync(id);
             if(existingItem is null){
                 return NotFound();
             }
-            repository.DeleteItem(id);
+            await repository.DeleteItemAsync(id);
 
             return NoContent();
         }
--- catalog/Settings/MongoDbSettings.cs
+++ catalog/Settings/MongoDbSettings.cs
@@ -9,6 +9,8 @@ namespace catalog.Settings
     {
         public string Host { get; set; }
         public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
 
         public string ConnectionString { 
             get {
